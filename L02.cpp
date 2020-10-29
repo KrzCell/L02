b6e3526ef43d21676b02ff882873572db9f2777d
Krzysztof Cel <krzycel789@student.polsl.pl>
2020-10-29 09:27:51 +0100

    początek

---
+++ L02.cpp
@@ -0,0 +1,16 @@
+﻿// L02.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+
+#define Zadanie2A
+#ifdef Zadanie2A
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+#endif // Zadanie2A
+
+
