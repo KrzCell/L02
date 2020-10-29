f13eb4bca659ea9ebb10402bcf66e18adcc4e98e
Krzysztof Cel <krzycel789@student.polsl.pl>
2020-10-29 09:33:49 +0100

    zmienne

--- L02.cpp
+++ L02.cpp
@@ -2,13 +2,23 @@
 //
 
 #include <iostream>
+#include <cmath>
+using namespace std;
 
 #define Zadanie2A
-#ifdef Zadanie2A
 
+#ifdef Zadanie2A
 int main()
+
 {
-    std::cout << "Hello World!\n";
+	double f = 0, x = 0, a = 0, b = 0;
+	cout << "Podaj wartosc x = ";
+	cin >> x;
+	cout << "Podaj wartosc a = ";
+	cin >> a;
+	cout << "Podaj wartosc b = ";
+	cin >> b;
+
 }
 
 #endif // Zadanie2A
